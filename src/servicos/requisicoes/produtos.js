f7ac5c550f9d27e2069f638e17655ae9d36540c1
GabrielyMachado <gabriely.machado@hotmail.com>
2023-01-13 21:01:45 -0300

    Criada web API para salvar as informações do carrinho

---
+++ src/servicos/requisicoes/produtos.js
@@ -0,0 +1,25 @@
+import api from "../api";
+
+export async function salvarProduto (produto){
+    try{
+        const resultado = await api.post('/produtos', produto)
+        return resultado.data;
+    }
+    catch(error){
+        console.error(error);
+        return {}
+    }
+    
+}
+
+export async function pegarProdutos (){
+    try{
+        const resultado = await api.get('/produtos');
+        return resultado.data;
+    }
+    catch(error){
+        console.error(error);
+        return []
+    }
+    
+}
